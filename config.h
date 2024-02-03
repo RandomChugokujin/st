424f7931b8b37f187e5fa395c07d736f7d4b10c3
brian <randomchugokujin@tutamail.com>
2024-02-02 20:45:05 -0500

    Add new font

--- config.h
+++ config.h
@@ -6,8 +6,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "hack:size=14:antialias=true:autohint=true";
-static char *font2[] = { "fontawesome:style=Solid:pixelsize=14:antialias=true:autohint=true",
-                         "fontawesomebrands:style=Solid:pixelsize=14:antialias=true:autohint=true",
+static char *font2[] = { //"fontawesome:style=Solid:pixelsize=14:antialias=true:autohint=true",
+                         //"fontawesomebrands:style=Solid:pixelsize=14:antialias=true:autohint=true",
+                         "Symbols Nerd Font:size=14:antialias=true",
                          "emoji:style=Solid:pixelsize=14:antialias=true:autohint=true"};
 static int borderpx = 1;
 
